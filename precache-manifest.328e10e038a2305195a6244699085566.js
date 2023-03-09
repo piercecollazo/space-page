c9a13f8eaabad016beedf2c2a713712e78a8b383
piercecollazo <piercecollazo@gmail.com>
2023-03-08 23:03:21 -0500

    Deploying to github pages

---
+++ precache-manifest.328e10e038a2305195a6244699085566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d56939031f4768ba91e951c8b12d6dd",
+    "url": "/space-page/index.html"
+  },
+  {
+    "revision": "5092930a1711cd1959b5",
+    "url": "/space-page/static/css/main.c739a6c8.chunk.css"
+  },
+  {
+    "revision": "86d7f7fadf98b6c74187",
+    "url": "/space-page/static/js/2.e8dd4544.chunk.js"
+  },
+  {
+    "revision": "5092930a1711cd1959b5",
+    "url": "/space-page/static/js/main.6104cc4a.chunk.js"
+  },
+  {
+    "revision": "2e966873932201b91ea6",
+    "url": "/space-page/static/js/runtime~main.80189aba.js"
+  },
+  {
+    "revision": "5443e3f1f0eb6ce86df47cc2697e387b",
+    "url": "/space-page/static/media/Earth.5443e3f1.mp4"
+  }
+]);
